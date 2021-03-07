3a143e06f3299394fb11578c1941910bcf5599c9
이동영 <leedy2491@ajou.ac.kr>
2021-03-08 03:28:02 +0900

    Add Pagination

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Navbar from './components/views/Navbar/Menu';
-import Products from './components/views/Products/Products'
+import ProductsPage from './components/views/Products/ProductsPage'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 const App = () => {
   return (
     <Router>
       <Navbar />
       <Switch>
-        <Route path='/products' exact component={Products} />
+        <Route path='/products' exact component={ProductsPage} />
       </Switch>
     </Router>
   );
