8c9d5b3c864eb33bba607e56015767b4004d8f36
이동영 <leedy2491@ajou.ac.kr>
2021-03-08 20:43:42 +0900

    Add Copyright

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/views/Navbar/Menu';
 import ProductsPage from './components/views/Products/ProductsPage'
 import CartPage from './components/views/Cart/CartPage'
+import Copyright from './components/views/Copyright/Copyright'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path='/products' exact component={ProductsPage} />
         <Route path='/cart' exact component={CartPage} />
       </Switch>
+      <Copyright />
     </Router>
   );
 };
---
+++ src/components/views/Copyright/Copyright.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Copyright.css";
+
+const Copyright = () => {
+  return (
+    <div>
+      <div className="CopyrightStyle">© {new Date().getFullYear()} DEV2D0 all rights reserved.</div>
+    </div>
+  );
+};
+export default Copyright;
